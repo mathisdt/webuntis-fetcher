672ab25935d4b9214fc9047ccaee1e4716a13f26
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2024-09-15 22:17:30 +0200

    add ability to fetch messages from inside WebUntis and forward them as emails

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ whenever Untis decides to change their backend.
 
 ## Getting Started
 
-1. Checkout this project to your computer (or just copy `main.py` and `config-template.ini`).
+### Fetch Timetable
+
+1. Checkout this project to your computer (or just copy `*.py` and `config-template.ini`).
 2. Copy `config-template.ini` to `config.ini` and edit it so it contains your data.
    - The section names (inside the `[]`) are used as titles for the time tables.
    - The `server` field has to be set to whatever your school uses. You can see the server
@@ -31,3 +33,28 @@ whenever Untis decides to change their backend.
    inside it (recommended).
 4. Run `main.py` periodically, which will write the output to `output.html` in the same
    directory as the script - or provide a target filename as argument to the script.
+
+### Fetch Messages
+
+1. Checkout this project to your computer (or just copy `*.py` and `config-template.ini`).
+2. Copy `config-template.ini` to `config.ini` and edit it so it contains your data.
+   - The section names (inside the `[]`) are used as titles for the time tables.
+   - The `server` field has to be set to whatever your school uses. You can see the server
+     name in the browser address bar when you are logged in, it's everything up to the first `/`. 
+   - The `school` field has to contain whatever Untis calls the school. You can look it up
+     by logging into your WebUntis account in a web browser and click the RSS icon on the
+     "Today" page (small, orange). The link you're directed to contains the right name
+     (look at the address bar of the browser).
+   - The `class` field is not used to access the backend, but setting it (to any value)
+     switches to "student" mode. If `class` is unset, the "teacher" mode is used, and class
+     names are displayed next to the period subjects.
+   - `message_id_file` should point to any writable file name. It does not have
+     to exist.
+   - `mail_from`, `mail_to` and `mail_host` are self-explanatory. `mail_to` can contain
+     multiple addresses separated by a semicolon.
+3. Make sure that at least Python 3.9 and the necessary libraries are installed
+   (e.g. by executing `pip3 install -r requirements.txt`). You can also set up a
+   [venv](https://docs.python.org/3/library/venv.html) and install the requirements
+   inside it (recommended).
+4. Run `messages.py` periodically, perhaps with the name of a different configuration file
+   as argument. It will send emails as configured.
---
+++ messages.py
@@ -0,0 +1,115 @@
+import configparser
+import locale
+import logging
+import os
+import sys
+import csv
+import smtplib
+from email.message import EmailMessage
+import mimetypes
+
+import requests
+
+if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        stream=sys.stdout)
+    if len(sys.argv) > 1:
+        configfile = sys.argv[1]
+    else:
+        configfile = f"{os.path.realpath(os.path.dirname(__file__))}/config.ini"
+    logging.debug(f'starting messages run - using configuration from {configfile}')
+
+    if not os.path.isfile(configfile):
+        logging.log(logging.ERROR, f"{configfile} not found")
+        exit(1)
+    config = configparser.ConfigParser()
+    config.read(configfile)
+
+    for section in config:
+        if section != 'DEFAULT':
+            if "message_id_file" not in config[section]:
+                logging.log(logging.ERROR, f"message_id_file not configured for {section}")
+                exit(2)
+            already_read_messages = list()
+            if os.path.isfile(config[section]["message_id_file"]):
+                with open(config[section]["message_id_file"], newline='') as message_id_file:
+                    message_id_reader = csv.reader(message_id_file, delimiter=',')
+                    for row in message_id_reader:
+                        already_read_messages.append(int(row[0]))
+
+            try:
+                response_initial = requests.get(
+                    f'{config[section]["server"]}/WebUntis/?school={config[section]["school"]}')
+                cookies = response_initial.cookies
+                requests.post(f'{config[section]["server"]}/WebUntis/j_spring_security_check', cookies=cookies,
+                              params={"school": config[section]["school"],
+                                      "j_username": config[section]["username"],
+                                      "j_password": config[section]["password"],
+                                      "token": ""})
+                token_new_response = requests.get(
+                    f'{config[section]["server"]}/WebUntis/api/token/new',
+                    cookies=cookies)
+                headers = {"Authorization": f"Bearer {token_new_response.text}"}
+                messages_response = requests.get(f'{config[section]["server"]}/WebUntis/api/rest/view/v1/messages',
+                                                 cookies=cookies,
+                                                 headers=headers)
+                messages = messages_response.json()
+
+                for msg in messages["incomingMessages"]:
+                    if not msg["id"] in already_read_messages:
+                        print(f'UNREAD  {msg["id"]} - {msg["subject"]}')
+
+                        message_response = requests.get(
+                            f'{config[section]["server"]}/WebUntis/api/rest/view/v1/messages/{msg["id"]}',
+                            cookies=cookies,
+                            headers=headers)
+                        msg_details = message_response.json()
+                        msg_attachments = dict()
+                        if "storageAttachments" in msg_details and msg_details["storageAttachments"]:
+                            for att in msg_details["storageAttachments"]:
+                                storageurl_response = requests.get(
+                                    f'{config[section]["server"]}/WebUntis/api/rest/view/v1/messages/{att["id"]}/attachmentstorageurl',
+                                    cookies=cookies,
+                                    headers=headers)
+                                storage_json = storageurl_response.json()
+                                download_url = storage_json["downloadUrl"]
+                                amazon_headers = dict()
+                                for header_entry in storage_json["additionalHeaders"]:
+                                    amazon_headers[header_entry["key"]] = header_entry["value"]
+                                download_response = requests.get(download_url,
+                                                                 cookies=cookies,
+                                                                 headers=amazon_headers)
+                                msg_attachments[att["name"]] = download_response.content
+
+                        mail = EmailMessage()
+                        mail.set_content(msg_details["content"])
+                        for att in msg_attachments:
+                            mimetype = mimetypes.guess_type(url=att)
+                            if "/" in mimetype[0]:
+                                splitted = mimetype[0].split("/")
+                                mime1 = splitted[0]
+                                mime2 = splitted[1]
+                            else:
+                                mime1 = mimetype[0]
+                                mime2 = mimetype[1]
+                            mail.add_attachment(msg_attachments[att], maintype=mime1, subtype=mime2,
+                                                filename=att)
+
+                        mail['Subject'] = f'[{msg["sender"]["displayName"]}] {msg["subject"]}'
+                        mail['From'] = config[section]["mail_from"]
+                        mail['To'] = config[section]["mail_to"]
+
+                        s = smtplib.SMTP(config[section]["mail_host"])
+                        s.send_message(mail)
+                        s.quit()
+
+                        already_read_messages.append(msg["id"])
+
+                with open(config[section]["message_id_file"], 'w', newline='') as message_id_file:
+                    message_id_writer = csv.writer(message_id_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                    for already_read_message_id in already_read_messages:
+                        message_id_writer.writerow([already_read_message_id])
+
+            except requests.RequestException as re:
+                pass
